025e4ab3389928f07382601ac90fcf2dc85a957c
Bhanuprasad17 <bhanuprasadsuram0018@gmail.com>
2025-03-11 11:45:44 +0530

    reverse list updated with all edge cases

--- problems/listProblems/reverseList.py
+++ problems/listProblems/reverseList.py
@@ -1,13 +1,25 @@
 # List reverse without using built-in methods
 
-my_list = [1,2,3,4,5]
+def reverse_list(my_list):
+    # If the list is empty or has only one element
+    if len(my_list) <= 1:
+        return my_list
 
-n = len(my_list)
+    n = len(my_list)
 
-for i in range(0,n//2):
-    # my_list[i],my_list[n-i-1] = my_list[n-i-1],my_list[i]
-    temp = my_list[i]
-    my_list[i] = my_list[n-i-1]
-    my_list[n-i-1] = temp
+    for i in range(0, n // 2):
+        temp = my_list[i]
+        my_list[i] = my_list[n - i - 1]
+        my_list[n - i - 1] = temp
 
-print(my_list)    
+    return my_list
+
+
+print("Reversed List:", reverse_list([1, 2, 3, 4, 5]))    
+print("Reversed List:", reverse_list([1, 2, 3, 4]))       
+print("Reversed List:", reverse_list([1]))                
+print("Reversed List:", reverse_list([]))                  
+print("Reversed List:", reverse_list([5, 4, 3, 2, 1]))    
+print("Reversed List:", reverse_list([1, 1, 1, 1, 1]))    
+print("Reversed List:", reverse_list([-1, -2, -3, -4]))    
+print("Reversed List:", reverse_list([1, -2, 3, -4]))   
